9eeb43acfb162d5b19e16c0c6f05305835e2feeb
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-04 02:42:08 +0000

    Answer to task 6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,11 @@ def hash_password(password: str) -> bytes:
     pwd_bytes = password.encode('utf-8')
     hashed = bcrypt.hashpw(pwd_bytes, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks two passwords and returns True or False"""
+    valid = False
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        valid = True
+    return valid

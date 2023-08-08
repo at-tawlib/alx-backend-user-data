adfeb5f8303605377be5cb5504f5b87b89e4b035
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-08 23:28:01 +0000

    Implement require authentication in file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,31 @@ class Auth:
     """"Class to manage the API Authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """desc"""
-        return False
+        """"
+        Defines which routes does not need authentication
+        Returns true if route requires authentication
+        Return:
+            boolean: true if path is not in excluded_paths else
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        # handle if path does not  end with  "/"
+        for _path in excluded_paths:
+            if _path.startswith(path):
+                return False
+            elif path.startswith(_path):
+                return False
+            elif _path[-1] == "*":
+                if path.startswith(_path[:-1]):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """description"""

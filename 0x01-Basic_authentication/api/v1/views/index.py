70af5aadaaa3aa8faf2ad43a8345f4d44cef70ce
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-08 22:44:24 +0000

    Implement foribidden route

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
     Raises abort(401)
     """
     abort(401, "Unauthorized")
+
+
+@app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    handles forbidden access of users
+    Raises abourt(403)
+    """
+    abort(403, "forbidden")

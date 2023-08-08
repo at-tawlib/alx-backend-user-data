507742782905f5972a6200f5ded937a38c7adf79
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-08 10:17:33 +0000

    Implement require_path of auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,10 +10,32 @@ class Auth:
     """Class to manage the API Authentication"""
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"desc"""
-        return False
+        """"
+        Defines which routes does not need authentication
+        Returns true if route requires authentication
+        Return:
+            boolean: true if path is not in excluded_paths else
+        """
+        if path is None:
+            return True
 
-    
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        # handle if path does not  end with  "/"
+        for _path in excluded_paths:
+            if _path.startswith(path):
+                return False
+            elif path.startswith(_path):
+                return False
+            elif _path[-1] == "*":
+                if path.startswith(_path[:-1]):
+                    return False
+        return True
+
     def authorization_header(self, request=None) -> str:
         """desc"""
         return None
@@ -21,4 +43,3 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """desc"""
         return None
-    

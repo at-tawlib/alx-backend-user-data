4870ce0473b29906c520f62a2b1bc908ac097ba8
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-10 18:13:35 +0000

    Add files from 0x01-Basic_authentication

---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+"""
+Basic auth
+"""
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+from models.user import User
+import base64
+
+
+class BasicAuth(Auth):
+    """Basic Authentication class"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """"
+        Return: Base64 part of the Authorization header
+        for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        # return value after "Basic" of the header text
+        return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Basic - Base64 decode
+        returns the decoded value of a Base64 string
+        Return:
+             decoded value of a Base64 string
+             None: if base64_authorization_header is None
+                 or not a string or not a valid base64
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            encoded = base64_authorization_header.encode("utf-8")
+            decoded = base64.b64decode(encoded)
+            return decoded.decode("utf-8")
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        extract User credentials from decoded authorization header
+        returns the user email and password from the Base64 decoded value
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        # if ":" not in decoded_base64_authorization_header:
+        #     return None, None
+        extracted = decoded_base64_authorization_header.split(":", 1)
+        return extracted[0], extracted[1]
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+        creates and returns a User instance based on the email and password
+        Return:
+            User instance
+        """
+        if user_email is None or user_pwd is None:
+            return None
+        if not isinstance(user_email, str) or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retreives the User instance"""
+        auth = self.authorization_header(request)
+        if auth is not None:
+            token = self.extract_base64_authorization_header(auth)
+
+            if token is not None:
+                decoded = self.decode_base64_authorization_header(token)
+
+                if decoded is not None:
+                    email, password = self.extract_user_credentials(decoded)
+
+                    if email is not None:
+                        return self.user_object_from_credentials(
+                            email, password)
+        return

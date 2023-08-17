e905c2fd1ba9ea8898385dd352e278c7d84b9534
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-17 02:28:17 +0000

    Add register user route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,19 +2,31 @@
 """
 Basic Flask App
 """
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 from auth import Auth
 
 
 app = Flask(__name__)
-AuTH = Auth()
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
 def home() -> str:
     """home route"""
     return jsonify({"message": "Bienvenue"})
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """end-point to register a user"""
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        try:
+            register = AUTH.register_user(email, password)
+            return ({"email": email, "message": "user created"}), 200
+        except ValueError:
+            return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":

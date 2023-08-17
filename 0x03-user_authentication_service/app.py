97985656df57ac1471f9843998ceec0efadb8e3d
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-17 08:05:42 +0000

    Remove strict_slashes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,7 +44,7 @@ def login() -> str:
         abort(401)
 
 
-@app.route("/sessions", methods=["DELETE"], strict_slashes=False)
+@app.route("/sessions", methods=["DELETE"])
 def logout() -> str:
     """
     find user with the session_id, if user exists,

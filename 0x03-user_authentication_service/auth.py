37495ff6a6a5ebf16f12b85a159faa2b88bc1b40
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-08-17 14:44:38 +0000

    Check if session_id is None in get_user_from_se

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -69,6 +69,8 @@ class Auth:
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """"Returns the corresponding user to the session_id"""
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
